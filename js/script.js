7cb27cc7b3e214403bdf2b3fff5f71cea675f12e
LiaSG <liashergill@gmail.com>
2019-09-11 12:30:48 +0100

    Trial & error with javascript
    Trying to make text toggle between letters and the pronounciation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
+// $(document).ready(function(){
+//   $("div.letter").click(function(){
+//     $(this).hide();
+//   });
+// });
 
+// function myFunction() {
+//   var x = document.getElementById("myDIV");
+//   if (x.innerHTML === "Ura") {
+//     x.innerHTML = "Swapped text!";
+//   } else {
+//     x.innerHTML = "Hello";
+//   }
+// }
