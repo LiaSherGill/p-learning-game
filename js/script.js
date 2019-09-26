ad57fe2ab368a6d219b41436403327ecb158ff9f
LiaSG <liashergill@gmail.com>
2019-09-26 11:25:29 +0100

    Best javascript solution so far

--- js/script.js
+++ js/script.js
@@ -12,3 +12,32 @@
 //     x.innerHTML = "Hello";
 //   }
 // }
+
+// jQuery(document).ready(function(){
+//     jQuery('#hideshow').on('click', function(event) {
+//         jQuery('#content').toggle('show');
+//     });
+// });
+
+// $(document).ready(function(){
+//   $("button").click(function(){
+//     $("p").toggle();
+//   });
+// });
+
+// $("div.letter").click(function(){
+//    $("div.pronounciation").show();
+
+// function clicked_on(){
+//         document.getElementById('future-clicked').innerHTML = 'Future World';
+//     }
+
+$("#button").on("click", function() {
+  var el = $(this);
+  if (el.text() == el.data("text-swap")) {
+    el.text(el.data("text-original"));
+  } else {
+    el.data("text-original", el.text());
+    el.text(el.data("text-swap"));
+  }
+});
